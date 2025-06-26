5eade3b9a73ff8df5b3436907b040154a8d3ea6b
margotinjune <margotinjune@gmail.com>
2025-06-26 15:02:58 +0900

    feat: boardService, boardServiceImpl 비즈니스 로직 생성

--- src/main/java/com/lime/board/service/BoardService.java
+++ src/main/java/com/lime/board/service/BoardService.java
@@ -1,4 +1,37 @@
 package com.lime.board.service;
 
+import com.lime.board.model.BoardVo;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
+
+import java.util.List;
+
 public interface BoardService {
+
+  // 게시글 목록 조회
+  List<EgovMap> selectBoardList() throws Exception;
+
+  // 게시글 목록 조회(페이징)
+  List<EgovMap> selectBoardList(BoardVo boardVo) throws Exception;
+
+  // 게시글 총 개수 조회
+  int selectBoardListCount(BoardVo boardVo) throws Exception;
+
+  // 게시글 상세 조회
+  EgovMap selectBoardDetail(int boardSeq) throws Exception;
+
+  // 게시글 등록
+  boolean insertBoard(BoardVo boardVo) throws Exception;
+
+  // 게시글 수정
+  boolean updateBoard(BoardVo boardVo) throws Exception;
+
+  // 조회수 증가
+  boolean updateViewCount(int boardSeq) throws Exception;
+
+  // 게시글 삭제
+  boolean deleteBoard(int boardSeq) throws Exception;
+
+  // 게시글 다중 삭제
+  boolean deleteBoardList(List<Integer> boardSeqs) throws Exception;
+
 }
--- src/main/java/com/lime/board/service/BoardServiceImpl.java
+++ src/main/java/com/lime/board/service/BoardServiceImpl.java
@@ -1,4 +1,70 @@
 package com.lime.board.service;
 
-public class BoardServiceImpl {
+import com.lime.board.model.BoardVo;
+import com.lime.board.persistence.BoardDao;
+import egovframework.rte.fdl.cmmn.EgovAbstractServiceImpl;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service("boardService")
+public class BoardServiceImpl extends EgovAbstractServiceImpl implements BoardService {
+
+  private final BoardDao boardDao;
+
+  public BoardServiceImpl(BoardDao boardDao) {
+    this.boardDao = boardDao;
+  }
+
+  @Override
+  public List<EgovMap> selectBoardList() throws Exception {
+    return boardDao.selectBoardList();
+  }
+
+  @Override
+  public List<EgovMap> selectBoardList(BoardVo boardVo) throws Exception {
+    return boardDao.selectBoardList(boardVo);
+  }
+
+  @Override
+  public int selectBoardListCount(BoardVo boardVo) throws Exception {
+    return boardDao.selectBoardListCount(boardVo);
+  }
+
+  @Override
+  public EgovMap selectBoardDetail(int boardSeq) throws Exception {
+    return boardDao.selectBoardDetail(boardSeq);
+  }
+
+  @Override
+  public boolean insertBoard(BoardVo boardVo) throws Exception {
+    int result = boardDao.insertBoard(boardVo);
+    return result > 0;
+  }
+
+  @Override
+  public boolean updateBoard(BoardVo boardVo) throws Exception {
+    int result = boardDao.updateBoard(boardVo);
+    return result > 0;
+  }
+
+  @Override
+  public boolean updateViewCount(int boardSeq) throws Exception {
+    int result = boardDao.updateViewCount(boardSeq);
+    return result > 0;
+  }
+
+  @Override
+  public boolean deleteBoard(int boardSeq) throws Exception {
+    int result = boardDao.deleteBoard(boardSeq);
+    return result > 0;
+  }
+
+  @Override
+  public boolean deleteBoardList(List<Integer> boardSeqs) throws Exception {
+    int result = boardDao.deleteBoardList(boardSeqs);
+    return result > 0;
+  }
 }
