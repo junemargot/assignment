57f01eb4ccb200a74dc921fbb8e713b2fa44d0f1
margotinjune <margotinjune@gmail.com>
2025-06-26 13:28:47 +0900

    feat: boardVo, boardDao 데이터 계층 생성

--- src/main/java/com/lime/board/model/BoardVo.java
+++ src/main/java/com/lime/board/model/BoardVo.java
@@ -1,4 +1,72 @@
 package com.lime.board.model;
 
-public class BoardVo {
+import egovframework.example.sample.service.SampleDefaultVO;
+
+import java.time.LocalDate;
+
+public class BoardVo extends SampleDefaultVO {
+  private int boardSeq;
+  private String title;
+  private String writer;
+  private int viewCount;
+  private LocalDate regDate;
+
+  public BoardVo() {}
+
+  public BoardVo(String title, String writer) {
+    this.title = title;
+    this.writer = writer;
+    this.viewCount = 0;
+  }
+
+  public int getBoardSeq() {
+    return boardSeq;
+  }
+
+  public void setBoardSeq(int boardSeq) {
+    this.boardSeq = boardSeq;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getWriter() {
+    return writer;
+  }
+
+  public void setWriter(String writer) {
+    this.writer = writer;
+  }
+
+  public int getViewCount() {
+    return viewCount;
+  }
+
+  public void setViewCount(int viewCount) {
+    this.viewCount = viewCount;
+  }
+
+  public LocalDate getRegDate() {
+    return regDate;
+  }
+
+  public void setRegDate(LocalDate regDate) {
+    this.regDate = regDate;
+  }
+
+  @Override
+  public String toString() {
+    return "BoardVo{" +
+            "boardSeq=" + boardSeq +
+            ", title='" + title + '\'' +
+            ", writer='" + writer + '\'' +
+            ", viewCount=" + viewCount +
+            ", regDate=" + regDate +
+            '}';
+  }
 }
--- src/main/java/com/lime/board/persistence/BoardDao.java
+++ src/main/java/com/lime/board/persistence/BoardDao.java
@@ -1,4 +1,57 @@
 package com.lime.board.persistence;
 
-public class BoardDao {
+import com.lime.board.model.BoardVo;
+import egovframework.rte.psl.dataaccess.EgovAbstractMapper;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository("boardDao")
+public class BoardDao extends EgovAbstractMapper {
+
+  // 게시글 목록 조회
+  public List<EgovMap> selectBoardList() throws Exception {
+    return selectList("boardDao.selectBoardList");
+  }
+
+  // 게시글 목록 조회(페이징)
+  public List<EgovMap> selectBoardList(BoardVo boardVo) throws Exception {
+    return selectList("boardDao.selectBoardList", boardVo);
+  }
+
+  // 게시글 총 개수 조회
+  public int selectBoardListCount(BoardVo boardVo) throws Exception {
+    return selectOne("boardDao.selectBoardListCount", boardVo);
+  }
+
+  // 게시글 상세 조회
+  public EgovMap selectBoardDetail(int boardSeq) throws Exception {
+    return selectOne("boardDao.selectBoardDetail", boardSeq);
+  }
+
+  // 게시글 등록
+  public int insertBoard(BoardVo boardVo) throws Exception {
+    return insert("boardDao.insertBoard", boardVo);
+  }
+
+  // 게시글 수정
+  public int updateBoard(BoardVo boardVo) throws Exception {
+    return update("boardDao.updateBoard", boardVo);
+  }
+
+  // 조회수 증가
+  public int updateViewCount(int boardSeq) throws Exception {
+    return update("boardDao.updateViewCount", boardSeq);
+  }
+
+  // 게시글 삭제
+  public int deleteBoard(int boardSeq) throws Exception {
+    return delete("boardDao.deleteBoard", boardSeq);
+  }
+
+  // 게시글 다중 삭제
+  public int deleteBoardList(List<Integer> boardSeqs) throws Exception {
+    return delete("boardDao.deleteBoardList", boardSeqs);
+  }
 }
